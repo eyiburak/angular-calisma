a219700d6393833fc1d82524759dda6bff960c63
eyiburak <51834374+eyiburak@users.noreply.github.com>
2020-09-02 10:50:49 +0300

    -Lesson 1: Generate manuel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MoviesComponent } from './movies/movies.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
